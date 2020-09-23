e10a2d3e56117019836b13c448df31685eb86c14
Jules Moorhouse <julesthecoder@gmail.com>
2020-09-23 08:42:32 +0100

    New images + master setup

---
+++ DrawerNavigation/Astronomy/AstronomyMasterDetailPage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Astronomy.Pages;
+using Xamarin.Forms;
+
+namespace Astronomy
+{
+    public class AstronomyMasterDetailPage: MasterDetailPage
+    {
+        public AstronomyMasterDetailPage()
+        {
+            var master = new AstronomyMasterPage();
+
+            if (Device.RuntimePlatform == Device.iOS)
+            {
+                master.IconImageSource = ImageSource.FromFile("nav-menu-icon.png");
+            }
+            this.Master = master;
+
+            this.Detail = new NavigationPage(new AboutPage());
+
+            this.MasterBehavior = MasterBehavior.Popover;
+        }
+    }
+}
---
+++ DrawerNavigation/Astronomy/AstronomyMasterPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Astronomy
+{
+    public partial class AstronomyMasterPage : ContentPage
+    {
+        public AstronomyMasterPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
