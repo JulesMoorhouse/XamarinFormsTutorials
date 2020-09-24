6f4ecff9992103a1f945eeba548258147ecb3f9c
Jules Moorhouse <julesthecoder@gmail.com>
2020-09-24 09:07:55 +0100

    RendererCommunicate initial files.

---
+++ RendererCommunicate/XFDraw.Android/ShadowedLabelRenderer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Android.Content;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using XFDraw;
+using XFDraw.Droid;
+
+[assembly: ExportRenderer(typeof(ShadowedLabel), typeof(ShadowedLabelRenderer))]
+namespace XFDraw.Droid
+{
+    public class ShadowedLabelRenderer : LabelRenderer
+    {
+        public ShadowedLabelRenderer(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Label> e)
+        {
+            base.OnElementChanged(e);
+
+            Control.SetShadowLayer(10, 5, 5, Android.Graphics.Color.DarkGray);
+        }
+    }
+}
