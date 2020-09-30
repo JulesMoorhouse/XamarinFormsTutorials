f219c330c92108b1b47b2ae39192d863888e6624
Jules Moorhouse <julesthecoder@gmail.com>
2020-09-30 15:19:09 +0100

    Dual layout code changes

--- DualScreenInfo/FlagFacts/FlagDetailsPage.xaml.cs
+++ DualScreenInfo/FlagFacts/FlagDetailsPage.xaml.cs
@@ -2,11 +2,15 @@ using FlagData;
 using Xamarin.Forms;
 using System;
 using Xamarin.Essentials;
+using Xamarin.Forms.DualScreen;
+using System.ComponentModel;
 
 namespace FlagFacts
 {
     public partial class FlagDetailsPage : ContentPage
     {
+        bool DeviceIsSpanned => DualScreenInfo.Current.SpanMode != TwoPaneViewMode.SinglePane;
+
         private readonly FlagDetailsViewModel vm;
 
         public FlagDetailsPage()
@@ -33,6 +37,34 @@ namespace FlagFacts
         private void OnNext(object sender, EventArgs e)
         {
             vm.MoveToNextFlag();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            DualScreenInfo.Current.PropertyChanged += DualScreen_PropertyChanged;
+        }
+
+        protected override void OnDisappearing()
+        {
+            DualScreenInfo.Current.PropertyChanged -= DualScreen_PropertyChanged;
+            base.OnDisappearing();
+        }
+
+        private void DualScreen_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            UpdateLayouts();
+        }
+
+        async void UpdateLayouts()
+        {
+            if (DeviceIsSpanned)
+            {   // the detail view should never be showing when spanned
+                if (Navigation.NavigationStack.Count > 1)
+                {
+                    await Navigation.PopToRootAsync();
+                }
+            }
         }
     }
 }
